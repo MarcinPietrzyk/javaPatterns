872acd4831a5ebcfd1ac16101fdbd70514d685d0
Marcin Pietrzyk <mlppietrzyk@gmail.com>
2018-04-02 19:06:49 +0200

    2nd pyramid commit

--- src/Patterns/Pyramids.java
+++ src/Patterns/Pyramids.java
@@ -2,7 +2,7 @@ package Patterns;
 
 public class Pyramids {
     public static void main(String[] args) {
-        System.out.println("Patterns");
+        System.out.println("1st patterns");
 
         for(int row = 1; row<=5;row++){
             for(int column = 1; column<=row;column++){
@@ -10,5 +10,15 @@ public class Pyramids {
             }
             System.out.print("\n");
         }
+
+        System.out.println("2nd pattern");
+
+        for(int row=1; row<=5;row++){
+            for(int column = row; column<=5;column++){
+                System.out.printf("*");
+            }
+            System.out.print("\n");
+        }
     }
+
 }
