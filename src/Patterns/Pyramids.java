d2f3cf6292b9e0b129a56b225ba0508a49e1fbe0
Marcin Pietrzyk <mlppietrzyk@gmail.com>
2018-04-02 20:34:52 +0200

    4th pyramid commit

--- src/Patterns/Pyramids.java
+++ src/Patterns/Pyramids.java
@@ -32,6 +32,17 @@ public class Pyramids {
             System.out.printf("\n");
         }
 
+        System.out.println("4th pattern");
+        for(int row = 0; row<5; row++){
+            for(int column = row; column<4; column++){
+                System.out.printf(" ");
+            }
+            for(int columnb = 0; columnb <= row; columnb++){
+                System.out.printf("*");
+            }
+            System.out.printf("\n");
+        }
+
     }
 
 }
