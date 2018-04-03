54f89fb0e60921a553485b31c2343be4e025f3a3
Marcin Pietrzyk <mlppietrzyk@gmail.com>
2018-04-03 20:41:36 +0200

    5th commit half diamond

--- src/Patterns/Pyramids.java
+++ src/Patterns/Pyramids.java
@@ -43,6 +43,20 @@ public class Pyramids {
             System.out.printf("\n");
         }
 
+        System.out.println("5th pattern - Half Diamond");
+        for(int row = 1; row<=5; row++){
+            for(int column = 1; column<=row;column++){
+                System.out.printf("*"+" ");
+            }
+            System.out.println("");
+        }
+        for(int row = 1; row<=4; row++){
+            for(int column = 4;column>=row;column--){
+                System.out.printf("* ");
+            }
+            System.out.println("");
+        }
+
     }
 
 }
